f2febdf5119393500dafd5eb70305aef159283d0
sharath1987 <150205015@svuca.edu>
2017-06-18 04:29:34 -0600

    test2.java is edited

--- test2.java
+++ test2.java
@@ -4,12 +4,18 @@ public class test2
 	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the range print the series:");
 		int n = input.nextInt();
 		int a=0, b=1;
 		for (int i=0;i<n;i++)
 		{
-			System.out.println(a);
-			a=a+i;
+			System.out.print(a+ "\t");
+			if (i%10==0)
+			System.out.println();
+			int c=a;
+			a=a+b;
+			b=c;
 		}
+		System.out.println();
 	}
 }
